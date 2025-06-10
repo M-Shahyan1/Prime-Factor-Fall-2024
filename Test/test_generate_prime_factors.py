7e8ce8e39f1859d4851d04a5d3ebce225e597d1d
Muhammad Shahyan <m.shahyan.edu@gmail.com>
2025-06-10 03:10:55 -0400

    Set up asd Task 1(value error for non-integers)

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,1 +1,5 @@
+from prime import generate_prime_factors
 
+
+def test_integer_check():
+    assert generate_prime_factors('X') == ValueError
--- prime.py
+++ prime.py
@@ -1,3 +1,11 @@
 """
 prime.py -- Write the application code here
 """
+
+
+def generate_prime_factors(int):
+    try:
+        int + 1
+        pass
+    except TypeError:
+        return ValueError
